0403ce304532764be55cacb96d4a64b03f45ebb0
Rafael Camoleze <rafricardo123@hotmail.com>
2023-01-09 13:57:58 -0300

    setup interface

---
+++ laravel/resources/views/components/footers/auth.blade.php
@@ -0,0 +1,15 @@
+<footer class="footer py-4  ">
+    <div class="container-fluid">
+        <div class="row align-items-center justify-content-lg-between">
+            <div class="col-lg-6 mb-lg-0 mb-4">
+                <div class="copyright text-center text-sm text-muted text-lg-start">
+                    © 2023,
+					{{__('made with')}} <i class="fa fa-heart"></i> {{__('by')}}
+                    <a href="https://github.com/camolezerafael" class="font-weight-bold" target="_blank">Rafael Camoleze</a> &
+                    <a href="https://rafaelcamoleze.com" class="font-weight-bold" target="_blank">Camoleze Tecnologia</a>
+					{{__(' for better health homes.')}}
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ laravel/resources/views/components/footers/guest.blade.php
@@ -0,0 +1,17 @@
+<footer class="footer position-absolute bottom-footer py-2 w-100 z-index-1">
+    <div class="container">
+        <div class="row align-items-center justify-content-lg-between">
+            <div class="col-12 col-md-12 my-auto">
+                <div class="copyright text-center text-sm text-white text-lg-start d-flex justify-content-center">
+					<span class="">
+						© 2023,
+						{{__('made with')}} <i class="fa fa-heart"></i> {{__('by')}}
+						<a href="https://github.com/camolezerafael" class="font-weight-bold link-secondary" target="_blank">Rafael Camoleze</a> &
+						<a href="https://rafaelcamoleze.com" class="font-weight-bold link-secondary" target="_blank">Camoleze Tecnologia</a>
+						{{__(' for better health homes.')}}
+					</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
