0497e351fc81df15a773902b545f2acb54cc2b3c
Rafael Camoleze <contato@rafaelcamoleze.com>
2024-01-15 23:16:37 -0300

    Secure Updates & Some Adjustements

--- laravel/resources/js/Components/ListPagination.jsx
+++ laravel/resources/js/Components/ListPagination.jsx
@@ -1,3 +1,5 @@
+import { Link } from '@inertiajs/react'
+
 export default function ListPagination( props ) {
 	const classNormal = 'relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-500 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0'
 	const classHidden = 'relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex'
@@ -8,18 +10,18 @@ export default function ListPagination( props ) {
 	return (
 		<div className="flex items-center justify-between border-t border-gray-200 bg-white px-2 pt-8 pb-3 sm:px-2">
 			<div className="flex flex-1 justify-between sm:hidden">
-				<a
+				<Link
 					href={props.prev_page_url}
 					className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
 				>
 					{props.links[0].label}
-				</a>
-				<a
+				</Link>
+				<Link
 					href={props.last_page_url}
 					className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
 				>
 					{props.links[props.last_page + 1].label}
-				</a>
+				</Link>
 			</div>
 			<div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
 				<div>
@@ -35,7 +37,7 @@ export default function ListPagination( props ) {
 							props.links.map((link, i) => {
 								if(i === 0){
 									return (
-										<a
+										<Link
 											key={i}
 											href={link.url}
 											className={ 'relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0' }
@@ -44,11 +46,11 @@ export default function ListPagination( props ) {
 											<svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
 												<path fillRule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clipRule="evenodd" />
 											</svg>
-										</a>
+										</Link>
 									)
 								}else if(i === indexLastLink){
 									return (
-										<a
+										<Link
 											key={i}
 											href={link.url}
 											className={ 'relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0' }
@@ -57,18 +59,18 @@ export default function ListPagination( props ) {
 											<svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
 												<path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
 											</svg>
-										</a>
+										</Link>
 									)
 								}else{
 									return (
-										<a
+										<Link
 											key={i}
 											href={link.url}
 											aria-current="page"
 											className={ link.active ? classCurrent : classNormal }
 										>
 											{link.label}
-										</a>
+										</Link>
 									)
 								}
 							})
