5c94e6c5641b9dfe4f398997d0e571be0c8aff4c
Rafael Camoleze <contato@rafaelcamoleze.com>
2024-09-05 22:36:31 -0300

    Many Updates

--- laravel/database/factories/TransactionFactory.php
+++ laravel/database/factories/TransactionFactory.php
@@ -27,7 +27,7 @@ class TransactionFactory extends Factory
 		return [
 			'transaction_type' => $this->faker->randomElement(['RECEI', 'FIXEX', 'VAREX', 'PEOPL', 'TAXES', 'TRANS']),
 			'description' => $this->faker->text(30),
-			'value' => $this->faker->randomFloat(0,1,999999),
+			'amount' => $this->faker->randomFloat(0,1,999999),
 			'status' => $this->faker->boolean(),
 			'date_due' => $this->faker->dateTimeInInterval('-1 year', '+1 year'),
 			'date_payment' => $this->faker->randomElement([$this->faker->dateTimeInInterval('-1 year', '+6 months'), null]),
